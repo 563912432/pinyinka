66da7ae6aabe2add5c677a12f666117ed5403b0d
陈帅 <1301491593@qq.com>
2018-04-26 11:30:03 +0800

    'sp'

--- src/components/introduce.vue
+++ src/components/introduce.vue
@@ -10,24 +10,46 @@
     name: 'introduce',
     data () {
       return {
+        host: '/',
         player: null
       }
     },
     mounted () {
-      let videoHeight = 200
-      let height = window.screen.height
-      let viewHeight = window.innerHeight
-      let titleHeight = height - viewHeight
-      let marginTop = (height - videoHeight) / 2 - titleHeight
-      document.getElementById('showVideo').style.marginTop = marginTop + 'px'
-      this.player = polyvObject('#showVideo').videoPlayer({
-        'width': '100%',
-        'height': videoHeight,
-        'vid': 'dbadf91f040cf6660e82900b204e9800_d'
+      let vid = 'dbadf91f040cf6660e82900b204e9800_d'
+      console.log(this.host + 'Api/Video/getPolySign/code/' + vid)
+      this.get(this.host + 'Api/Video/getPolySign/code/' + vid, res => {
+        if (res.status) {
+          let polySign = JSON.parse(res.info)
+          let videoHeight = 200
+          let height = window.screen.height
+          let viewHeight = window.innerHeight
+          let titleHeight = height - viewHeight
+          let marginTop = (height - videoHeight) / 2 - titleHeight
+          document.getElementById('showVideo').style.marginTop = marginTop + 'px'
+          this.player = polyvObject('#showVideo').videoPlayer({
+            'width': '100%',
+            'height': videoHeight,
+            'vid': vid,
+            'ts': polySign.ts,
+            'sign': polySign.sign
+          })
+          this.player.j2s_resumeVideo()
+        } else {
+          console.log(res.info)
+        }
       })
-      this.player.j2s_resumeVideo()
     },
     methods: {
+      get (url, fn) { // XMLHttpRequest对象用于在后台与服务器交换数据
+        let xhr = new XMLHttpRequest()
+        xhr.open('GET', url, true)
+        xhr.onreadystatechange = function () {
+          if (+xhr.readyState === 4 && (+xhr.status === 200 || +xhr.status === 304 || +xhr.status === 0)) { // readyState == 4说明请求已完成
+            fn.call(this, xhr.responseText) // 从服务器获得数据
+          }
+        }
+        xhr.send()
+      },
       outer () {
         // 关闭视频，关闭页面
         this.player.j2s_pauseVideo()
